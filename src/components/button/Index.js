014cebb16fdc0321ea606281f9f685d2c58a83e5
Harrison <fayvorharrison101@gmail.com>
2020-11-11 01:41:40 +0100

    build reusable like button component

---
+++ src/components/button/Index.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import "../../App.css";
+import { Badge } from 'react-bootstrap';
+import constants from '../../constants';
+
+const LikeButton = props => {
+  const [page, setPage] = useState({});
+  const [isActive, setActiveStatus] = useState(false);
+
+  const like = async () => {
+    const page = await fetch(`${constants.API_ENDPOINT}/api/page/${props.id}`, {
+      method: "PUT"
+    })
+      .then(resp => resp.json());
+
+    setActiveStatus(true);
+    setPage(page);
+  };
+
+  useEffect(() => {
+    const fetchPageInfo = async () => {
+      const page = await fetch(`${constants.API_ENDPOINT}/api/page/${props.id}`)
+        .then(resp => resp.json());
+
+      setPage(page);
+    };
+
+    fetchPageInfo();
+  }, [props.id]);
+
+  return (
+    // <div className="button__wrapper">
+    <button className={`like__button ${isActive ? 'active' : ''}`} onClick={like}>
+      <span id="thumbs-up" className="far fa-4x fa-thumbs-up"></span>
+      <Badge variant="light">{page.noOfLikes || 0}</Badge>
+      <span className="sr-only">number of likes</span>
+    </button>
+    // </div>
+  )
+}
+
+export default LikeButton;
